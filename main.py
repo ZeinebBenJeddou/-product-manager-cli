7f4bdfd3591bd659cbcc5e1bfbf2a806ea770483
Zeineb_BenJeddou <zeinebbenjeddou01@gmail.com>
2026-09-15 10:15:51 +0100

    Ajoute la creation de produits

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print("=== PRODUCT MANAGER CLI ===")
-print("Application démarrée")
+
+
+def add_product(products):
+    nom=input("Nom : ")
+    prix=float(input("Prix : "))
+    stock=int(input("Stock : "))
+    dictionnaire={
+    "name": nom,
+    "price": prix,
+    "stock": stock
+}
+    products.append(dictionnaire)
+
+
+products = []
+add_product(products)
+add_product(products)
+print(products)
