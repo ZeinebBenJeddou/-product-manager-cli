d7baf0d7140a20938b3e31d662bbe7bd4f92cfba
Zeineb_BenJeddou <zeinebbenjeddou01@gmail.com>
2026-09-15 10:27:55 +0100

    Ajoute la recherche de produits

--- main.py
+++ main.py
@@ -16,7 +16,13 @@ def display_products(products):
     for product in products:
         print(nb+1,".", product["name"],"-", product["price"],"DT - Stock",product["stock"])
         nb=nb+1
-        
+
+def search_product(products,name):
+    for p in products:
+        if p["name"]==name:
+            return p
+    return None
+
 
 
 
@@ -25,3 +31,6 @@ products = [
     {"name": "p2", "price": 556, "stock": 4}
 ]
 display_products(products)
+
+print(search_product(products, "p2"))
+print(search_product(products, "p5"))
