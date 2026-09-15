c7582bdb7c6d2cdf98e12ed6fa22c753dbb44c57
Zeineb_BenJeddou <zeinebbenjeddou01@gmail.com>
2026-09-15 11:17:44 +0100

    Ajoute la validation des produits

--- main.py
+++ main.py
@@ -2,14 +2,30 @@
 
 def add_product(products):
     nom=input("Nom : ")
-    prix=float(input("Prix : "))
-    stock=int(input("Stock : "))
+    if nom.strip() == "":
+        print("Nom invalide")
+        return False
+    try:
+
+        prix=float(input("Prix : "))
+        stock=int(input("Stock : "))
+    except ValueError:
+        print("Prix ou stock invalide") 
+        return False
+    if prix <= 0:
+        print("Le prix doit être supérieur à 0.")
+        return False
+
+    if stock < 0:
+        print("Le stock ne peut pas être négatif.")
+        return False
     dictionnaire={
     "name": nom,
     "price": prix,
     "stock": stock
 }
     products.append(dictionnaire)
+    return True
 
 def display_products(products):
     nb=0
@@ -23,14 +39,33 @@ def search_product(products,name):
             return p
     return None
 
+def update_stock(products, name, new_stock):
+    produit=search_product(products,name)
+    if produit!=None:
+        if new_stock>=0:
+            produit["stock"]=new_stock
+            return True
+    return False
 
+def delete_product(products, name):
+    produit=search_product(products,name)
+    if produit!=None:
+        del products[products.index(produit)]
+        return True
 
+    return False
+
+def calculate_inventory_value(products):
+    val=0
+    for p in products:
+        val= val+(p["price"]*p["stock"])
+    return val
 
 products = [
-    {"name": "p1", "price": 520.2, "stock": 5},
-    {"name": "p2", "price": 556, "stock": 4}
 ]
+
+add_product(products)
+
 display_products(products)
 
-print(search_product(products, "p2"))
-print(search_product(products, "p5"))
+print(calculate_inventory_value(products))
